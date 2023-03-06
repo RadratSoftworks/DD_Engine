86d03e9f8c76e264076ee0d60a57d7701bc61433
Do Trong Thu <lmt42@yandex.com>
2023-03-06 12:45:34 +0700

    Add about button

---
+++ Assets/Scripts/Installer/AboutButtonController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace DDEngine.Installer
+{
+    public class AboutButtonController : MonoBehaviour
+    {
+        [SerializeField]
+        private AboutPopupController popupController;
+
+        public void OnClick()
+        {
+            popupController.Show();
+        }
+    }
+}
---
+++ Assets/Scripts/Installer/AboutPopupController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+namespace DDEngine.Installer
+{
+    public class AboutPopupController : MonoBehaviour
+    {
+        [SerializeField]
+        private CanvasGroup canvasGroup;
+
+        [SerializeField]
+        private float fadeDuration = 0.3f;
+
+        private void Awake()
+        {
+            DOTween.Init();
+        }
+
+        private void Start()
+        {
+            OnEnable();
+        }
+
+        private void OnEnable()
+        {
+            canvasGroup.DOFade(1.0f, fadeDuration);
+        }
+
+        public void OnClick()
+        {
+            canvasGroup.DOFade(0.0f, fadeDuration)
+                .OnComplete(() => gameObject.SetActive(false));
+        }
+
+        public void Show()
+        {
+            gameObject.SetActive(true);
+        }
+    }
+}
